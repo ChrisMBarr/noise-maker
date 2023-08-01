e46fa7dd5d7baa54dbd0d186a823e430da1936d1
FiniteLooper <463685+FiniteLooper@users.noreply.github.com>
2023-08-01 18:23:41 +0000

    Deploying to gh-pages from @ FiniteLooper/noise-maker@8111570df52132a7c2a5541abf05a9c5b33b4a96 🚀

---
+++ helpers.js
@@ -0,0 +1,64 @@
+function forceReloadSvg() {
+  //Re-select every time since it's being replaced and we lose the reference
+  const $svg = $('#demo-output svg');
+
+  //remove any styles it has
+  if ($svg.css('transform')) {
+    $svg.css('transform', '');
+  }
+
+  //clone the element and replace it
+  var $clone = $svg.clone();
+  $svg.replaceWith($clone);
+
+  //force it into a new GPU rendering layer
+  $clone.css('transform', 'translateZ(0)');
+}
+
+function updateTextureFilter($tgt, tgtFilterProp, val, isDisabled) {
+  textureStyles.filter[tgtFilterProp] = isDisabled ? null : val;
+  $tgt.css('filter', getPropsAsCssString(textureStyles.filter));
+}
+
+function getPropsAsCssString(obj) {
+  return Object.keys(obj)
+    .map((key) => {
+      const val = obj[key];
+      return val == null ? '' : `${key}(${val})`;
+    })
+    .filter((v) => v !== '')
+    .join(' ');
+}
+
+function clearLightingEffects() {
+  $('#noise-filter').find('feDiffuseLighting, feSpecularLighting').remove();
+}
+
+function createLightingElement() {
+  const $svgFilter = $('#noise-filter');
+  const result = $svgFilter.find('feTurbulence').attr('result');
+  const lightingPrimitiveType = $('#ctrl-lighting-primitive-type').val();
+
+  const lightingPrimitiveEl = document.createElementNS(svgNs, lightingPrimitiveType);
+  lightingPrimitiveEl.setAttributeNS(svgNs, 'in', result);
+  lightingPrimitiveEl.appendChild(document.createTextNode('\n'));
+  lightingPrimitiveEl.appendChild(getLightElement());
+  lightingPrimitiveEl.appendChild(document.createTextNode('\n'));
+
+  $svgFilter.append('\n').append(lightingPrimitiveEl).append('\n');
+}
+
+function getLightElement() {
+  const lightType = $('#ctrl-light-type').val();
+  return document.createElementNS(svgNs, lightType);
+}
+
+function replaceLightElement() {
+  const $svgFilter = $('#noise-filter');
+  $svgFilter.find('feDistantLight, fePointLight, feSpotLight').remove();
+  $svgFilter
+    .find('feDiffuseLighting, feSpecularLighting')
+    .append('\n')
+    .append(getLightElement())
+    .append('\n');
+}
