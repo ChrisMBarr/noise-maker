b6dd02878355538f650bdf3370472fe264530e2f
FiniteLooper <463685+FiniteLooper@users.noreply.github.com>
2023-08-05 18:48:45 +0000

    Deploying to gh-pages from @ FiniteLooper/noise-maker@404c39a2bed5b633f0a2eac1072f640ffdbc5617 🚀

--- helpers.js
+++ helpers.js
@@ -1,9 +1,11 @@
+let $handles;
+
 let forceReloadDebounce = null;
 function forceReloadSvg() {
   clearTimeout(forceReloadDebounce);
   forceReloadDebounce = setTimeout(() => {
     //Re-select every time since it's being replaced and we lose the reference
-    const $svg = $('#demo-output svg');
+    const $svg = $demoOutput.children('svg');
 
     //remove any styles it has
     if ($svg.css('transform')) {
@@ -23,9 +25,13 @@ let lightingMaxDebounce = null;
 function updateLightingMaxValues() {
   clearTimeout(lightingMaxDebounce);
   lightingMaxDebounce = setTimeout(() => {
-    const $svg = $('#demo-output svg');
+    const $svg = $demoOutput.children('svg');
     let maxX = $svg.width();
     let maxY = $svg.height();
+
+    canvasSize.height = maxY;
+    canvasSize.width = maxX;
+    canvasSize.left = $svg.offset().left;
 
     $(
       '#ctrl-lighting-point-x, #ctrl-lighting-spot-overhead-x, #ctrl-lighting-spot-manual-x, #ctrl-lighting-spot-manual-pointsat-x'
@@ -74,7 +80,8 @@ function createLightingElement() {
 }
 
 function getLightElement() {
-  const lightType = $('#ctrl-light-type').val();
+  const selectedVal = $('#ctrl-light-type').val();
+  const lightType = $(`#ctrl-light-type option[value="${selectedVal}"]`).data('light-type');
   return document.createElementNS(svgNs, lightType);
 }
 
@@ -88,23 +95,79 @@ function replaceLightElement() {
     .append('\n');
 }
 
+function clearLightingHandles() {
+  isDraggingHandle = false;
+  $demoOutput.children('.handle').remove();
+  $handles = $demoOutput.children('.handle'); //should select nothing, which is what we want here
+}
+
+function createLightHandles(handleMappings) {
+  clearLightingHandles();
+
+  if (handleMappings.length > 0) {
+    handleMappings.forEach((data) => {
+      $('<div class="handle bg-light rounded-circle border border-secondary" tabindex="0"></div>')
+        .data('mapping', data)
+        .insertBefore($demoOutput.children('svg'));
+    });
+
+    $handles = $demoOutput.children('.handle');
+
+    let mapping;
+    let $dragHandle;
+    $demoOutput
+      .on('mousedown', (ev) => {
+        if (ev.target.classList.contains('handle')) {
+          isDraggingHandle = true;
+          $dragHandle = $(ev.target);
+          mapping = $dragHandle.data('mapping');
+        }
+      })
+      .on('mousemove', (ev) => {
+        if (isDraggingHandle) {
+          const x = Math.min(
+            Math.max(ev.originalEvent.clientX - canvasSize.left, 0),
+            canvasSize.width
+          );
+          const y = Math.min(Math.max(ev.originalEvent.clientY, 0), canvasSize.height);
+
+          $('#' + mapping.left)
+            .val(x)
+            .trigger(inputEventName);
+          $('#' + mapping.top)
+            .val(y)
+            .trigger(inputEventName);
+
+          $dragHandle.css({ left: x, top: y });
+        }
+      })
+      .on('mouseup', () => {
+        isDraggingHandle = false;
+        mapping = undefined;
+        $dragHandle = undefined;
+      });
+  }
+}
+
+function updateHandlePosition(index, positionProperty, value) {
+  // console.log($handles.eq(index), positionProperty, value);
+  $handles.eq(index).css(positionProperty, value + 'px');
+}
+
 $(() => {
   $('.dropdown-toggle').on('click', (ev) => {
     const $menu = $(ev.target).siblings('.dropdown-menu');
-    $(ev.target).add($menu).addClass('show');
+    $(ev.target).add($menu).toggleClass('show');
   });
 
   $(document).on('click', (ev) => {
     const $clicked = $(ev.target);
-
-    console.log($clicked);
-
     if (
-      $clicked.parents('.dropdown').length === 0 ||
+      $clicked.parents('.dropdown, .dropup').length === 0 ||
       $clicked.parents('.dropdown-menu').length === 1
     ) {
       // close the dropdown
-      $('.dropdown .show').removeClass('show');
+      $('.dropdown .show, .dropup .show').removeClass('show');
     }
   });
 });
