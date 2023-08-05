69bd3e26b5f31285975cbcd4ae45b92f10caeaaf
FiniteLooper <chris.m.barr@gmail.com>
2023-08-05 11:03:43 -0400

    Split out init code into separate functions

--- src/main.js
+++ src/main.js
@@ -23,96 +23,104 @@ $(() => {
 
       //Checkboxes to enable/disable other inputs
       if ($enableInput.length) {
-        const $enableTargets = $($enableInput.data('enable'));
-        $enableInput.on(inputEventName, () => {
-          const isChecked = $enableInput.is(':checked');
-          $enableTargets.attr('disabled', !isChecked);
-
-          if ($enableInput.attr('id') === 'ctrl-enable-lighting') {
-            lightingEffectsEnabled = isChecked;
-            if (isChecked) {
-              createLightingElement();
-            } else {
-              clearLightingEffects();
-            }
-          } else if ($enableInput.attr('id') === 'ctrl-enable-custom-size') {
-            customSizeEnabled = isChecked;
-            updateLightingMaxValues();
-            if (!isChecked) {
-              $('#demo-output svg').css({ height: '100%', width: '100%' });
-            }
-          }
-
-          $enableTargets.each((_i, t) => updateTexture($(t), $outputDisplay));
-          $enableTargets.trigger(inputEventName);
-        });
-
-        //Initialize
-        $enableInput.trigger(inputEventName);
+        initEnableInputs($enableInput, $outputDisplay);
       }
 
+      //Checkboxes or <select> options that show/hide other elements
       if ($toggleVisibilityInput.length) {
-        if ($toggleVisibilityInput.is(':checkbox')) {
-          const $toggleTargets = $($toggleVisibilityInput.data('toggle-visibility'));
-          $toggleVisibilityInput.on(inputEventName, () => {
-            $toggleTargets.toggle($toggleVisibilityInput.is(':checked'));
+        initToggleVisibilityInputs($toggleVisibilityInput, $outputDisplay);
+      }
 
-            if ($toggleVisibilityInput.attr('id') === 'ctrl-separate-frequencies') {
-              updateTexture($baseFrequencyX, $outputDisplay);
-            }
-          });
-          //Initialize
-          $toggleTargets.toggle($toggleVisibilityInput.is(':checked'));
-        } else if ($toggleVisibilityInput.is('select')) {
-          const $allToggles = $toggleVisibilityInput.find(
-            'option[data-toggle-visibility-and-enable]'
-          );
-          const allTargetsSelectorStr = $allToggles
-            .toArray()
-            .map((x) => {
-              return $(x).data('toggle-visibility-and-enable');
-            })
-            .join(',');
-          const $allTargets = $(allTargetsSelectorStr);
+      //All other form inputs
+      if ($input.length) {
+        initFormInputs($input, $outputDisplay);
+      }
+    });
 
-          $toggleVisibilityInput.on(inputEventName, (ev) => {
-            const $currentTarget = $toggleVisibilityInput.children().eq(ev.target.selectedIndex);
-            const $toggleTargets = $($currentTarget.data('toggle-visibility-and-enable'));
+  function initEnableInputs($enableInput, $outputDisplay) {
+    const $enableTargets = $($enableInput.data('enable'));
+    $enableInput.on(inputEventName, () => {
+      const isChecked = $enableInput.is(':checked');
+      $enableTargets.attr('disabled', !isChecked);
 
-            const id = $toggleVisibilityInput.attr('id');
-            if (lightingEffectsEnabled) {
-              if (id === 'ctrl-lighting-primitive-type') {
-                clearLightingEffects();
-                createLightingElement();
-                $('#lighting-controls .shared-lighting-controls')
-                  .find('input, select:not(#' + id + ')')
-                  .trigger(inputEventName);
-              } else if (id === 'ctrl-light-type') {
-                replaceLightElement();
-              }
-            }
-
-            $allTargets.hide().find('input, select').attr('disabled', 'disabled');
-            const $enabledInputs = $toggleTargets
-              .show()
-              .find('input, select')
-              .removeAttr('disabled');
-            $enabledInputs.each((_i, t) => updateTexture($(t), $outputDisplay));
-            $enabledInputs.trigger(inputEventName);
-          });
+      if ($enableInput.attr('id') === 'ctrl-enable-lighting') {
+        lightingEffectsEnabled = isChecked;
+        if (isChecked) {
+          createLightingElement();
+        } else {
+          clearLightingEffects();
+        }
+      } else if ($enableInput.attr('id') === 'ctrl-enable-custom-size') {
+        customSizeEnabled = isChecked;
+        updateLightingMaxValues();
+        if (!isChecked) {
+          $('#demo-output svg').css({ height: '100%', width: '100%' });
         }
       }
 
-      //Form inputs
-      if ($input.length) {
-        $input.on(inputEventName, () => {
-          updateTexture($input, $outputDisplay);
-        });
-
-        //Initialize
-        $input.trigger(inputEventName);
-      }
+      $enableTargets.each((_i, t) => updateTexture($(t), $outputDisplay));
+      $enableTargets.trigger(inputEventName);
     });
+
+    //Initialize
+    $enableInput.trigger(inputEventName);
+  }
+
+  function initToggleVisibilityInputs($toggleVisibilityInput, $outputDisplay) {
+    if ($toggleVisibilityInput.is(':checkbox')) {
+      const $toggleTargets = $($toggleVisibilityInput.data('toggle-visibility'));
+      $toggleVisibilityInput.on(inputEventName, () => {
+        $toggleTargets.toggle($toggleVisibilityInput.is(':checked'));
+
+        if ($toggleVisibilityInput.attr('id') === 'ctrl-separate-frequencies') {
+          updateTexture($baseFrequencyX, $outputDisplay);
+        }
+      });
+      //Initialize
+      $toggleTargets.toggle($toggleVisibilityInput.is(':checked'));
+    } else if ($toggleVisibilityInput.is('select')) {
+      const $allToggles = $toggleVisibilityInput.find('option[data-toggle-visibility-and-enable]');
+      const allTargetsSelectorStr = $allToggles
+        .toArray()
+        .map((x) => {
+          return $(x).data('toggle-visibility-and-enable');
+        })
+        .join(',');
+      const $allTargets = $(allTargetsSelectorStr);
+
+      $toggleVisibilityInput.on(inputEventName, (ev) => {
+        const $currentTarget = $toggleVisibilityInput.children().eq(ev.target.selectedIndex);
+        const $toggleTargets = $($currentTarget.data('toggle-visibility-and-enable'));
+
+        const id = $toggleVisibilityInput.attr('id');
+        if (lightingEffectsEnabled) {
+          if (id === 'ctrl-lighting-primitive-type') {
+            clearLightingEffects();
+            createLightingElement();
+            $('#lighting-controls .shared-lighting-controls')
+              .find('input, select:not(#' + id + ')')
+              .trigger(inputEventName);
+          } else if (id === 'ctrl-light-type') {
+            replaceLightElement();
+          }
+        }
+
+        $allTargets.hide().find('input, select').attr('disabled', 'disabled');
+        const $enabledInputs = $toggleTargets.show().find('input, select').removeAttr('disabled');
+        $enabledInputs.each((_i, t) => updateTexture($(t), $outputDisplay));
+        $enabledInputs.trigger(inputEventName);
+      });
+    }
+  }
+
+  function initFormInputs($input, $outputDisplay) {
+    $input.on(inputEventName, () => {
+      updateTexture($input, $outputDisplay);
+    });
+
+    //Initialize
+    $input.trigger(inputEventName);
+  }
 
   //Initialize max values & update them on window resize
   $(window).on('resize', updateLightingMaxValues);
